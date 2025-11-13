5cb189e520a14ce37de577be9e9db267b2f56a0b
Alamin Islam <alamin648890@gmail.com>
2025-11-13 12:14:35 +0600

    featured artworks fetch and show in the home ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 import Slider from "../components/Slider";
 import AboutSection from "../components/About";
+import FeaturedArtworks from "../components/FeaturedArtworks";
 
 export async function getFeatured() {
   const res = await fetch("http://localhost:3000/arts/featured");
   if (!res.ok) throw new Error("Failed to fetch featured artworks");
   const data = await res.json();
-  console.log(data)
   return { data };
 }
 
@@ -35,6 +35,7 @@ export default function Home() {
     <div>
       <Slider/>
       <AboutSection/>
+      <FeaturedArtworks items={items} loading = {busy}/>
     </div>
   );
 }
